af82f27ca0a9b0430ee7f2a237f2c657fe36122c
asatake <asataken@gmail.com>
2019-01-19 23:47:14 +0900

    :up: react-router-domの@typesを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Main from './pages/Main';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Main from "./pages/Main";
+import registerServiceWorker from "./registerServiceWorker";
 import "./assets/index.scss";
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<Main />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
