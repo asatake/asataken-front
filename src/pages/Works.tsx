6608a043d3e4914086c0f283f671dc2005d4b0a2
asatake <asataken@gmail.com>
2019-01-21 23:02:29 +0900

    :sparkles: Worksの雛形を作成

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,15 +1,65 @@
 import * as React from "react";
-import { Paper, Typography } from "@material-ui/core";
+import { Typography, Tabs, Tab, Card, FormControl, InputLabel, Select } from "@material-ui/core";
 
-class Works extends React.Component {
+interface IWorksProps {}
+
+interface IWorksState {
+  tabItem: number;
+}
+
+class Works extends React.Component<IWorksProps, IWorksState> {
+  constructor(props: IWorksProps) {
+    super(props);
+    this.state = {
+      tabItem: 0
+    }
+    this.changeTab = this.changeTab.bind(this);
+  }
+
+  public changeTab(event: any, tabItem: number) {
+    this.setState({ tabItem });
+  }
+
+  public request() {
+    return (
+      <Card>
+        aaa
+      </Card>
+    );
+  }
+
+  public discography() {
+    return (
+      <Card>
+        <form autoComplete="off">
+        <FormControl>
+          <InputLabel>Year</InputLabel>
+          <Select native={true}>
+            <option>2019</option>
+          </Select>
+        </FormControl>
+        </form>
+        <Typography component="p" variant="h6">
+          準備中です。更新をお待ちください。
+        </Typography>
+      </Card>
+    );
+  }
+
   public render() {
     return (
       <div>
-        <Paper>
-          <Typography variant="overline" component="p">
-            準備中
-          </Typography>
-        </Paper>
+        <Tabs
+          value={this.state.tabItem}
+          onChange={this.changeTab}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="ご依頼に関して" />
+          <Tab label="今までの活動" />
+        </Tabs>
+        {this.state.tabItem === 0 && <Card>{this.request()}</Card>}
+        {this.state.tabItem === 1 && <Card>{this.discography()}</Card>}
       </div>
     );
   }
