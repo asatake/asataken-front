d15a49884328ea86f3d4ff9acfb0af66a079fa42
asatake <asataken@gmail.com>
2019-01-24 22:59:05 +0900

    :up: Typographyのチェックをを次世代のものにできるよう設定追加

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,15 +1,17 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import {green, deepOrange} from '@material-ui/core/colors';
-
+import { createMuiTheme } from "@material-ui/core/styles";
+import { green, deepOrange } from "@material-ui/core/colors";
 
 export const theme = createMuiTheme({
-    palette: {
-      type: 'light',
-      primary: {
-        main: green[800]
-      },
-      secondary: {
-        main: deepOrange[800]
-      },
+  palette: {
+    type: "light",
+    primary: {
+      main: green[800]
     },
-  });
+    secondary: {
+      main: deepOrange[800]
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
