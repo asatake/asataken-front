2580d732445bde8d5621354a7059a5d00d50741e
asatake <asataken@gmail.com>
2019-02-05 19:24:56 +0900

    :green_heart: デプロイできるように設定変更

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+fatal() {
+  echo $@
+  exit 1
+}
+
+# ssh command wrap
+remote() {
+  ssh -q -o "StrictHostKeyChecking no" -i deploy_rsa -l ${SFTP_USER} -p ${SFTP_PORT} www.asatake.com $@
+}
+
+[ -z "$SFTP_HOST" ] && fatal 'SFTP_HOST is not set'
+[ -z "$SFTP_PORT" ] && fatal 'SFTP_PORT is not set'
+
+# copy build
+tar cvf - build | remote tar xvf -
+
+remote <<EOF
+  set -e
+  mv build /srv/www/
+EOF
