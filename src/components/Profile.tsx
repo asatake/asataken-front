7da4248722b34eefc4f2c2deee6471e473520b66
asatake <asataken@gmail.com>
2019-02-08 16:40:29 +0900

    :cop: 別タブで開くリンクのrelにnoreffererを追加
    サイトを操られないようにするため

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Card, List, ListItem, ListItemText, Divider } from '@material-ui/core';
-import { Person, Cake } from '@material-ui/icons';
+import { Card, List, ListItem, ListItemText, Divider } from "@material-ui/core";
+import { Person, Cake } from "@material-ui/icons";
 import MdiIcon from "@mdi/react";
 import { mdiTwitter, mdiSoundcloud } from "@mdi/js";
 
@@ -24,7 +24,8 @@ class Profile extends React.Component {
               button={true}
               component="a"
               href="https://twitter.com/Mush_asatake"
-              target="blank"
+              target="_blank"
+              rel="norefferer"
             >
               <MdiIcon path={mdiTwitter} size="1.5em" color="#1da1f2" />
               <ListItemText secondary="@Mush_asatake" />
@@ -34,7 +35,8 @@ class Profile extends React.Component {
               button={true}
               component="a"
               href="https://soundcloud.com/mush_asatake"
-              target="blank"
+              target="_blank"
+              rel="norefferer"
             >
               <MdiIcon path={mdiSoundcloud} size="1.5em" color="#ff3300" />
               <ListItemText secondary="mush_asatake" />
